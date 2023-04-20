438542b042649ea7127c23239ff81e748fcd3d31
TomasMistina <xmistinat@stuba.sk>
2023-04-21 00:38:19 +0200

    added basic ver of name, email and password modals

--- app/main/account/profile.js
+++ app/main/account/profile.js
@@ -1,5 +1,5 @@
 import { COLORS, SIZES } from '../../../constants/theme'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 
 import React from 'react'
 import { SafeAreaView } from 'react-native'
@@ -8,6 +8,12 @@ import { UserContext } from '../../_layout'
 
 const profile = () => {
     const { currentUser, setCurrentUser } = React.useContext(UserContext)
+    const [nameModalVisible, setNameModalVisible] = useState(false)
+    const [emailModalVisible, setEmailModalVisible] = useState(false)
+    const [passwordModalVisible, setPasswordModalVisible] = useState(false)
+    const [profileName, setProfileName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <SafeAreaView
             style={{ flex: 1, backgroundColor: COLORS.backdrop, gap: 20 }}
@@ -35,13 +41,137 @@ const profile = () => {
                 </Text>
             </View>
             <View>
-                <TouchableOpacity style={styles.button} onPress={() => {}}>
+                {/* Change Name Modal */}    
+                <Modal
+                    animationType="slide"
+                    visible={nameModalVisible}
+                    transparent={true}
+                    onRequestClose={() => {
+                        setNameModalVisible(!nameModalVisible)
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalText}>
+                                Change Name
+                            </Text>
+                            <TextInput 
+                                style={styles.default}
+                                placeholder="New profile name"
+                                value={profileName}
+                                onChangeText={setProfileName}
+                                placeholderTextColor={COLORS.secondary}
+                                >
+                            </TextInput>
+                            <Text style={[styles.textStyle, styles.buttonText]}>
+                                Confirm changes
+                            </Text>
+                            <TouchableOpacity
+                                style={styles.buttonStyle}
+                                onPress={() =>
+                                    setNameModalVisible(!nameModalVisible)
+                                }
+                            >
+                                <Text style={styles.buttonText}>Go back</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </Modal>
+                <TouchableOpacity
+                    style={[styles.default, styles.buttonWithIcon]}
+                    onPress={() => setNameModalVisible(true)}
+                >
+                    <FolderIcon size={24} color={COLORS.secondary} />
                     <Text style={styles.textStyle}>Change Name</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => {}}>
+
+                {/* Change E-mail Modal */}
+                <Modal
+                    animationType="slide"
+                    visible={emailModalVisible}
+                    transparent={true}
+                    onRequestClose={() => {
+                        setEmailModalVisible(!emailModalVisible)
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalText}>
+                                Change E-mail
+                            </Text>
+                            <TextInput 
+                                style={styles.default}
+                                placeholder="New e-mail"
+                                value={email}
+                                onChangeText={setEmail}
+                                placeholderTextColor={COLORS.secondary}
+                                keyboardType="email-address"
+                                >
+                            </TextInput>
+                            <Text style={[styles.textStyle, styles.buttonText]}>
+                                Confirm changes
+                            </Text>
+                            <TouchableOpacity
+                                style={styles.buttonStyle}
+                                onPress={() =>
+                                    setEmailModalVisible(!emailModalVisible)
+                                }
+                            >
+                                <Text style={styles.buttonText}>Go back</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </Modal>
+                <TouchableOpacity
+                    style={[styles.default, styles.buttonWithIcon]}
+                    onPress={() => setEmailModalVisible(true)}
+                >
+                    <FolderIcon size={24} color={COLORS.secondary} />
                     <Text style={styles.textStyle}>Change Email</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => {}}>
+
+                {/* Change Password Modal */}
+                <Modal
+                    animationType="slide"
+                    visible={passwordModalVisible}
+                    transparent={true}
+                    onRequestClose={() => {
+                        setPasswordModalVisible(!passwordModalVisible)
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalText}>
+                                Change Name
+                            </Text>
+                            <TextInput 
+                                style={styles.default}
+                                placeholder="Password"
+                                secureTextEntry={true}
+                                value={password}
+                                onChangeText={setPassword}
+                                placeholderTextColor={COLORS.secondary}
+                                >
+                            </TextInput>
+                            <Text style={[styles.textStyle, styles.buttonText]}>
+                                Confirm changes
+                            </Text>
+                            <TouchableOpacity
+                                style={styles.buttonStyle}
+                                onPress={() =>
+                                    setPasswordModalVisible(!passwordModalVisible)
+                                }
+                            >
+                                <Text style={styles.buttonText}>Go back</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </Modal>
+                <TouchableOpacity
+                    style={[styles.default, styles.buttonWithIcon]}
+                    onPress={() => setPasswordModalVisible(true)}
+                >
+                    <FolderIcon size={24} color={COLORS.secondary} />
                     <Text style={styles.textStyle}>Change Password</Text>
                 </TouchableOpacity>
             </View>
