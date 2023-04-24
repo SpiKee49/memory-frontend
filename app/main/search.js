85b39985e9163a898e35d80ede779cdf5aa80cf2
TomasMistina <xmistinat@stuba.sk>
2023-04-24 17:22:52 +0200

    functionality in search added

--- app/main/search.js
+++ app/main/search.js
@@ -1,28 +1,111 @@
-import { FlatList, SafeAreaView, View } from 'react-native'
-import React, { useState } from 'react'
+import {
+    ActivityIndicator,
+    FlatList,
+    SafeAreaView,
+    Text,
+    View,
+} from 'react-native'
+import { COLORS, SIZES } from '../../constants/theme'
+import React, { useEffect, useState } from 'react'
 
+import { API_URL } from '@env'
 import AlbumCard from '../../components/AlbumCard'
-import { COLORS } from '../../constants/theme'
 import SearchBar from '../../components/SearchBar'
+import axios from 'axios'
 import { useRouter } from 'expo-router'
 
 const search = () => {
     const [searchValue, setSearchValue] = useState('')
     const router = useRouter()
+
+    const [albums, setAlbums] = useState()
+
+    useEffect(() => {
+        fetchAlbums()
+    }, [])
+
+    useEffect(() => {
+        fetchAlbums(searchValue)
+    }, [searchValue])
+
+    const fetchAlbums = async (search) => {
+        try {
+            const res = await axios.get(
+                !search
+                    ? `${API_URL}/api/albums`
+                    : `${API_URL}/api/albums?search=${search}`
+            )
+            setAlbums(res.data)
+        } catch (error) {
+            console.error(
+                `Error received from axios.post: ${JSON.stringify(error)}`
+            )
+        }
+    }
+
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.backdrop }}>
-            <SearchBar onChange={setSearchValue} value={'Search for album'} />
-            <View>
-                <FlatList
-                    data={[1, 2, 3, 4, 5]}
-                    renderItem={({ item }) => (
-                        <AlbumCard
-                            onPress={() => router.push(`/main/album/${item}`)}
+            <SearchBar
+                onChange={setSearchValue}
+                debounce
+                value={'Search for album'}
+            />
+            {albums == undefined ? (
+                <ActivityIndicator
+                    size="large"
+                    color={COLORS.secondary}
+                    style={{ marginTop: 20 }}
+                />
+            ) : (
+                <View>
+                    {albums.length === 0 ? (
+                        <View
+                            style={{
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                marginBottom: 40,
+                                gap: 5,
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    color: '#fff',
+                                    fontSize: SIZES.xxl,
+                                    fontWeight: 'bold',
+                                    paddingHorizontal: 40,
+                                    paddingBottom: 5,
+                                    borderBottomWidth: 1,
+                                    borderBottomColor: COLORS.secondary,
+                                }}
+                            >
+                                No match
+                            </Text>
+                            <Text
+                                style={{
+                                    color: COLORS.secondary,
+                                    fontSize: SIZES.md,
+                                    fontStyle: 'italic',
+                                }}
+                            >
+                                try different search value
+                            </Text>
+                        </View>
+                    ) : (
+                        <FlatList
+                            data={albums}
+                            renderItem={({ item }) => (
+                                <AlbumCard
+                                    album={item}
+                                    onPress={() =>
+                                        router.push(`/main/album/${item.id}`)
+                                    }
+                                />
+                            )}
+                            keyExtractor={(item) => item.id}
                         />
                     )}
-                    keyExtractor={(item) => item}
-                />
-            </View>
+                </View>
+            )}
         </SafeAreaView>
     )
 }
